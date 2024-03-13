8d49ba293bd28f63da335fab260e4e67bb8f5684
Renan Moraes <106771076+sohrenan@users.noreply.github.com>
2024-03-13 19:41:11 -0300

    Changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,10 @@
 import pandas as pd
-from mcdm.ahp import AHPGaussiano
 import streamlit as st
-import os
+from mcdm.ahp import AHPGaussiano
+
 
 def load_data():
-    current_dir = os.path.dirname(__file__)
-    return pd.read_csv(os.path.join(current_dir, 'bike_stats.csv'))
+    return pd.read_csv('data/bike_stats.csv')
 
 def display_header(title):
     st.markdown(f"<h1 style='text-align: center;font-size:40px;'>{title}</h1>", unsafe_allow_html=True)
@@ -16,12 +15,12 @@ def display_comparison_columns(bike_names):
     with col1:
         display_header("Moto 1")
         modelo_1 = st.selectbox('Selecione a primeira moto', bike_names)
-        preco_1 = st.number_input(f"Por qual preço você pretende comprar o primeiro modelo?")
+        preco_1 = st.number_input(f"Por qual preço você pretende comprar o primeiro modelo?",min_value=100)
 
     with col3:
         display_header("Moto 2")
         modelo_2 = st.selectbox('Selecione a segunda moto', bike_names)
-        preco_2 = st.number_input(f"Por qual preço você pretende comprar o segundo modelo?")
+        preco_2 = st.number_input(f"Por qual preço você pretende comprar o segundo modelo?",min_value=100)
 
     return modelo_1, preco_1, modelo_2, preco_2
 
@@ -41,7 +40,7 @@ def perform_comparison(df, modelo_1, preco_1, modelo_2, preco_2):
     dados = df[df['modelo'].isin([modelo_1, modelo_2])]
 
     # Add missing columns if not present
-    for col in ['cilindrada', 'potencia', 'torque', 'consumo', 'peso', 'tanque', criterio]:
+    for col in ['cilindrada', 'potencia', 'torque', 'peso', 'tanque', criterio]:
         if col not in dados.columns:
             dados[col] = None
 
@@ -59,21 +58,25 @@ def perform_comparison(df, modelo_1, preco_1, modelo_2, preco_2):
 
 def main():
     st.set_page_config(
-        page_title="Previsão Resultados CBLOL 2024",
-        page_icon="images/cblol.ico",  # Provide the path to your favicon
+        page_title="Calculadora de Custo Beneficio de Motocicletas 2024",
+
         layout="centered"
     )
+    st.image('images/background.jpg')
+
 
     df = load_data()
     bike_names = df['modelo'].tolist()
 
-    display_header("Moto Comparison")
+
+    display_header("Comparador de Custo/Beneficio de Motos")
     modelo_1, preco_1, modelo_2, preco_2 = display_comparison_columns(bike_names)
     predict_button = display_comparison_button()
 
     if predict_button:
         escolha_ahp_gaussiano = perform_comparison(df, modelo_1, preco_1, modelo_2, preco_2)
-        st.write(escolha_ahp_gaussiano)
+        st.write(f'Melhor escolha é {escolha_ahp_gaussiano.index[0]} com peso {escolha_ahp_gaussiano.iloc[0][1]}')
+        st.write(escolha_ahp_gaussiano, width=0)  # Set width to 0 to make it fill the available space
 
     hide_default_format = """
            <style>
